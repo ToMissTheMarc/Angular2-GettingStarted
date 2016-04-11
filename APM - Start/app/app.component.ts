b59b9a00fd199f234156ded18d6ab2ea5620a7b5
Marc Longoria <marclongoria@outlook.com>
2016-04-10 20:42:51 -0600

    First component

---
+++ APM - Start/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "angular2/core";
+
+
+@Component({
+    selector: 'pm-app',
+    template: '
+    <div><h1>{{pageTitle}}</h1>
+        <div>My first component</div>
+    </div>
+    '
+    
+})
+
+
+export class AppComponent{
+    pageTitle: "Marc's Website";
+    
+    
+}
